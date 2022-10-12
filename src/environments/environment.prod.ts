1bb001495158185f3dd409e09654f252864253f6
TedataEsystems <106475960+TedataEsystems@users.noreply.github.com>
2022-10-12 15:28:01 +0200

    نوع الجهاز

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 API_URL:'https://localhost:44395/'
-    //API_URL : 'http://172.29.29.9:4090/'
-   //API_URL : 'http://172.29.29.9:3091/'
+   // API_URL : 'http://172.29.29.9:4090/'
+  // API_URL : 'http://172.29.29.9:3091/'
 };
