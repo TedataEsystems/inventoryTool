d11e41e7202913c5c751cd8e00d230aae183db02
Abdelrahman Ali <abdo.ali1012@outlook.com>
2023-10-25 11:15:20 +0300

    update angular.json and other files

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/PscSystem'),
+      dir: require('path').join(__dirname, './coverage/App3'),
       subdir: '.',
       reporters: [
         { type: 'html' },
